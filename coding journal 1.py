e63010c7b89cbf01b69ddc6221662f2c9ecf1a21
esa <mmgerard@ucsc.edu>
2025-10-07 14:24:52 -0700

    Add files via upload

---
+++ coding journal 1.py
@@ -0,0 +1,11 @@
+#session 1 prompt
+
+x = "My name is Esa Gerardo :)"
+y = " and my pronouns are she/her"
+print (x+y)
+
+favorite_movie = "My favorite movie is Kajillionaire because it's really weird, but also really heartfelt."
+print(favorite_movie)
+
+favorite_food = "My favorite food currently is probably cheeseburgers - they taste best when I eat them with my best friend!"
+print(favorite_food)

c3fc5b136b2fd6ffe500b7dbd58a32bd4aa077ae
esa <mmgerard@ucsc.edu>
2025-10-18 19:59:48 -0700

    Add files via upload

---
+++ session2codingjournal.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# #Session 2 Prompt: Write a Python that prints the sum of two floating point numbers, the difference between two integers, and the product of a floating point number and an integer. In each case, have the program print out the data type of the resulting answer.
+
+# In[4]:
+
+
+import numpy as np
+x = np.array([4.2])
+y = np.array([11.21])
+x+y
+
+
+# In[5]:
+
+
+import numpy as np
+x = np.array ([51])
+y = np.array ([17])
+x-y
+
+
+# In[6]:
+
+
+import numpy as np
+x = np.array ([12.2])
+y = np.array ([16])
+x*y
+
+
+# In[ ]:
+
+
+
+
